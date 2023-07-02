357f85ec0c9c44963cbffbb2bd973f386bfc652e
Christian Sohns <christiansohns@gmx.net>
2023-07-02 14:49:00 +0200

    Added German localization

---
+++ Recipes/Views/TagView.swift
@@ -0,0 +1,18 @@
+//
+//  TagView.swift
+//  Recipes
+//
+//  Created by Christian Sohns on 7/2/23.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagView()
+}

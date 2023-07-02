3c7a8548c75bed01c07cab69f45a48d244de3c72
Christian Sohns <christiansohns@gmx.net>
2023-07-02 12:23:15 +0200

    Ingredients can now be added easily and the instructions are now a multiline text field.

---
+++ Recipes/Models/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Recipes
+//
+//  Created by Christian Sohns on 7/2/23.
+//
+
+import Foundation
---
+++ Recipes/Views/EmptyListItem.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyListItem.swift
+//  Recipes
+//
+//  Created by Christian Sohns on 7/2/23.
+//
+
+import SwiftUI
+
+struct EmptyListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyListItem()
+}
